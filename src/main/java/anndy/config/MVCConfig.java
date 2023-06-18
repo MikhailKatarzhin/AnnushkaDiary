b72b3ecf7b6f5af59391380c5dab2641008e4a3c
mikhailkatarzhin <maikl.1997@mail.ru>
2023-06-19 00:18:36 +0400

    Главная страница с объяснением markdown

--- src/main/java/anndy/config/MVCConfig.java
+++ src/main/java/anndy/config/MVCConfig.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MVCConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("profile/profile");
-        registry.addViewController("/").setViewName("profile/profile");
-        registry.addViewController("/sign_in").setViewName("sign_in");
-        registry.addViewController("/sign_up").setViewName("sign_up");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/sign_in").setViewName("auth/sign_in");
+        registry.addViewController("/sign_up").setViewName("auth/sign_up");
     }
 }
